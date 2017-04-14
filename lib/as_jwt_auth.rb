79e5aea277c0d22b86ba3b15005ea133cf400e07
Amiel Martin and Kyle Rader <dev+amiel+kyle-rader@actionsprout.com>
2017-04-14 11:23:32 -0700

    Add generate_jwt method

--- lib/as_jwt_auth.rb
+++ lib/as_jwt_auth.rb
@@ -3,6 +3,10 @@ require "as_jwt_auth/version"
 module AsJWTAuth
   autoload :VerifyJWT, 'as_jwt_auth/verify_jwt'
 
+  def self.generate_jwt(payload = {}, key: )
+    JWT.encode payload, key, 'ES256'
+  end
+
   def self.verify_jwt(jwt, public_key: )
     verifier = VerifyJWT.new(public_key)
     verifier.valid? jwt
--- spec/as_jwt_auth_spec.rb
+++ spec/as_jwt_auth_spec.rb
@@ -5,19 +5,29 @@ RSpec.describe AsJWTAuth do
     expect(AsJWTAuth::VERSION).not_to be nil
   end
 
+  let(:private_key) do
+    key = OpenSSL::PKey::EC.new 'prime256v1'
+    key.generate_key
+  end
+
+  let(:public_key) do
+    key = OpenSSL::PKey::EC.new private_key
+    key.private_key = nil
+    key
+  end
+
+  describe '#generate_jwt' do
+    let(:jwt) {
+      described_class.generate_jwt 'the' => 'payload', key: private_key
+    }
+
+    it 'creates a JWT' do
+      payload, _headers = JWT.decode jwt, public_key
+      expect(payload).to eq 'the' => 'payload'
+    end
+  end
+
   describe '#verify_jwt' do
-
-    let(:private_key) do
-      key = OpenSSL::PKey::EC.new 'prime256v1'
-      key.generate_key
-    end
-
-    let(:public_key) do
-      key = OpenSSL::PKey::EC.new private_key
-      key.private_key = nil
-      key
-    end
-
     let(:payload) { {} }
     let(:jwt) { JWT.encode payload, private_key, 'ES256' }
 
