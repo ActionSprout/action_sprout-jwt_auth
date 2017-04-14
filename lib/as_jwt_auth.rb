f97525c46d14ecb6e4961a9071d5992ec5f2dd0e
Amiel Martin and Kyle Rader <dev+amiel+kyle-rader@actionsprout.com>
2017-04-13 17:19:17 -0700

    autoload VerifyJWT in module

--- lib/as_jwt_auth.rb
+++ lib/as_jwt_auth.rb
@@ -1,7 +1,8 @@
 require "as_jwt_auth/version"
-require 'as_jwt_auth/verify_jwt'
 
 module AsJWTAuth
+  autoload :VerifyJWT, 'as_jwt_auth/verify_jwt'
+
   def self.verify_jwt(jwt, public_key: )
     verifier = VerifyJWT.new(public_key)
     verifier.valid? jwt
