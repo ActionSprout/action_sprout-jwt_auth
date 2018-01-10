1b6424892a074e9bf69eec75be7b1652c6bb2c0a
Amiel Martin <amiel.martin@gmail.com>
2018-01-10 13:34:51 -0800

    Use as_jwt_auth for context

--- lib/as_jwt_auth/railtie.rb
+++ lib/as_jwt_auth/railtie.rb
@@ -1,14 +1,14 @@
 require 'rails/railtie'
 require 'as_jwt_auth/rails_controller_helpers'
 
-class Railtie < Rails::Railtie
+class AsJWTAuth::Railtie < Rails::Railtie
   config.after_initialize do |app|
     if Rails.env.test?
       require 'as_jwt_auth/test/rspec' if defined?(RSpec) && RSpec.respond_to?(:configure)
     end
   end
 
-  initializer 'draper.setup_action_controller' do
+  initializer 'as_jwt_auth.setup_action_controller' do
     ActiveSupport.on_load :action_controller do
       class_eval do
         include AsJWTAuth::RailsControllerHelpers
