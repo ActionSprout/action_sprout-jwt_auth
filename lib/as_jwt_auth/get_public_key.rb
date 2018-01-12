4ee07feaddadc07c55838a068da82a84db0f28d4
Amiel Martin <amiel.martin@gmail.com>
2018-01-11 16:16:39 -0800

    Refactor AsJWTAuth::GetPublicKey

--- lib/as_jwt_auth/get_public_key.rb
+++ lib/as_jwt_auth/get_public_key.rb
@@ -8,18 +8,43 @@ module AsJWTAuth
     method_object :jwt, http_client: HTTParty
 
     def call
-      auth_url_template = Addressable::Template.new(ENV.fetch('JWT_KEY_SERVER_URL_TEMPLATE'))
-      jwt_app_name = AsJWTAuth::JWTHeader.issuer jwt
       assert jwt_app_name, 'This JWT is missing an issuer'
-      jwt_for_request = AsJWTAuth.generate_jwt
-      url = auth_url_template.expand(issuer: jwt_app_name).to_s
-      response = http_client.get(url, headers: { 'X-Auth' => jwt_for_request })
+
+      response = make_request
+
       assert response.success?, "There was an issue loading the public key for #{jwt_app_name.inspect}"
-      response.parsed_response.dig(*%w[ data attributes key ])
+
+      key_from_response response
     end
+
+    private
 
     def assert(value, message)
       fail message unless value
+    end
+
+    def make_request
+      http_client.get public_key_url, headers: { 'X-Auth' => jwt_for_request }
+    end
+
+    def key_from_response(response)
+      response.parsed_response.dig(*%w[ data attributes key ])
+    end
+
+    def public_key_url
+      auth_url_template.expand(issuer: jwt_app_name).to_s
+    end
+
+    def auth_url_template
+      Addressable::Template.new ENV.fetch('JWT_KEY_SERVER_URL_TEMPLATE')
+    end
+
+    def jwt_app_name
+      @_jwt_app_name ||= AsJWTAuth::JWTHeader.issuer jwt
+    end
+
+    def jwt_for_request
+      @_jwt_for_request ||= AsJWTAuth.generate_jwt
     end
   end
 end
