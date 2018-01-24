835b8d9be6cc12e6a4a06689dc6c9ea6d6d0297d
Amiel Martin <amiel.martin@gmail.com>
2018-01-23 21:03:17 -0800

    Fix redefine const warning by moving it
    I'm seeing warnings like this in clients that use the testing helpers.
    
        /usr/local/bundle/gems/action_sprout-jwt_auth-0.4.1/lib/action_sprout/jwt_auth/test/rspec.rb:14: warning: already initialized constant NO_RESPONSE_FAILURE_MESSAGE
        /usr/local/bundle/gems/action_sprout-jwt_auth-0.4.1/lib/action_sprout/jwt_auth/test/rspec.rb:14: warning: previous definition of NO_RESPONSE_FAILURE_MESSAGE was here
        /usr/local/bundle/gems/action_sprout-jwt_auth-0.4.1/lib/action_sprout/jwt_auth/test/rspec.rb:14: warning: already initialized constant NO_RESPONSE_FAILURE_MESSAGE
        /usr/local/bundle/gems/action_sprout-jwt_auth-0.4.1/lib/action_sprout/jwt_auth/test/rspec.rb:14: warning: previous definition of NO_RESPONSE_FAILURE_MESSAGE was here

--- lib/action_sprout/jwt_auth/test/rspec.rb
+++ lib/action_sprout/jwt_auth/test/rspec.rb
@@ -1,5 +1,27 @@
 require 'action_sprout/jwt_auth/test/controller_helpers'
 
+NO_RESPONSE_FAILURE_MESSAGE = <<-MESSAGE.strip_heredoc
+  Using the shared example 'a JWT authorized request' requires that a request be made.
+
+  The request can either be in a `before` block, or defined as `let(:request)`.
+
+  For example:
+
+      it_should_behave_like 'a JWT authorized request' do
+        let(:response) { get :index }
+      end
+
+  Or:
+
+     context 'the index page' do
+       let(:response) { get :index }
+       it_should_behave_like 'a JWT authorized request'
+     end
+
+  If you are seeing this error and have defined a request, it is possible that your
+  request is raising an exception. Check rspec results for other exceptions.
+MESSAGE
+
 RSpec.configure do |config|
   config.include ActionSprout::JWTAuth::Test::ControllerHelpers, type: :controller
 
@@ -10,28 +32,6 @@ RSpec.configure do |config|
     before do
       set_jwt_auth jwt, jwt_result: jwt_result
     end
-
-    NO_RESPONSE_FAILURE_MESSAGE = <<-MESSAGE.strip_heredoc
-      Using the shared example 'a JWT authorized request' requires that a request be made.
-
-      The request can either be in a `before` block, or defined as `let(:request)`.
-
-      For example:
-
-          it_should_behave_like 'a JWT authorized request' do
-            let(:response) { get :index }
-          end
-
-      Or:
-
-         context 'the index page' do
-           let(:response) { get :index }
-           it_should_behave_like 'a JWT authorized request'
-         end
-
-      If you are seeing this error and have defined a request, it is possible that your
-      request is raising an exception. Check rspec results for other exceptions.
-    MESSAGE
 
     after do
       fail NO_RESPONSE_FAILURE_MESSAGE unless response.sent?
